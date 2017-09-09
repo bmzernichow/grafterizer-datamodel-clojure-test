bdb6f0f0d6c2103afd9414196fe177fea469d227
Bjørn Marius von Zernichow <BjornMarius.vonZernichow@sintef.no>
2017-09-09 12:34:00 +0200

    Refactored transformationdatamodel.js and generateclojure.js

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
+import * as transformationDataModel from '../assets/transformationdatamodel.js';
+import * as generateClojure from '../assets/generateclojure.js';
+import * as data from './data.json';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'app';
+  private title = 'Click to generate clojure';
+  generateClojure() {
+    console.log(generateClojure.fromTransformation(transformationDataModel.Transformation.revive(data)));
+    console.log(transformationDataModel.Transformation.revive(data));
+  };
 }
